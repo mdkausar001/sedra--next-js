b4504c451013f5dd9e73bf458c4edda3e8b89e93
MD SHAHBAZ ANSARI <MDSHAHBAZ8298@GMAIL.COM>
2025-05-17 15:09:04 +0530

    new updated of 17/05/2025

--- src/components/Contact/ContactUs.tsx
+++ src/components/Contact/ContactUs.tsx
@@ -14,11 +14,11 @@ const ContactUs = () => {
     }, []);
 
     return (
-        <div className="min-h-screen px-8 py-24 bg-gradient-to-bl from-[#1f4b76] to-[#011936] text-white font-sans"
-        // style={{
-        //     backgroundImage:
-        //         "url('https://sedra.net.sa/wp-content/uploads/2024/03/path-1.png')",
-        // }}
+        <div className="min-h-screen px-8 py-24 bg-white text-gray-700 font-sans"
+            style={{
+                backgroundImage:
+                    "url('https://sedra.net.sa/wp-content/uploads/2024/03/path-1.png')",
+            }}
         >
             <div className="py-10">
                 <h1 className="text-blue-700 mb-6 text-center text-4xl font-bold">
@@ -30,20 +30,20 @@ const ContactUs = () => {
             </div>
             <div className="max-w-6xl mx-auto grid md:grid-cols-2 gap-12 items-start">
                 {/* Left: Contact Info */}
-                <div className="gap gap-y-10">
-                    <h2 className="text-2xl md:text-3xl font-bold text-cyan-400 mb-6 tracking-widest">
+                <div className="gap gap-y-10" data-aos="fade-right">
+                    <h2 className="text-2xl md:text-3xl font-bold text-gray-700 mb-6 tracking-widest">
                         INFORMATION
                     </h2>
                     <div className="flex items-start gap-6 mb-4">
-                        <MapPin className="text-cyan-400 w-6 h-6" />
-                        <p className="mb-4 leading-relaxed text-gray-200">
+                        <MapPin className="text-cyan-600 w-6 h-6" />
+                        <p className="mb-4 leading-relaxed text-gray-700">
                             Kingdom of Saudi Arabia<br />
                             AL Madinah Al Monwarah street, <br /> Al Bawadi Dis, Al Madina Plaza Building,
                             Floor 5, Office 505
                         </p></div>
 
                     <div className="flex items-start gap-6 mb-4">
-                        <Phone className="text-cyan-400 w-6 h-6" />
+                        <Phone className="text-cyan-600 w-6 h-6" />
                         <p>
                             Tel: 00 966 114 229 922<br />
                             Fax: 00 966 11 211 411
@@ -51,19 +51,19 @@ const ContactUs = () => {
                     </div>
 
                     <div className="flex items-start gap-6 mb-4">
-                        <Mail className="text-cyan-400 w-6 h-6" />
+                        <Mail className="text-cyan-600 w-6 h-6" />
                         <p>info@sedra.net.sa</p>
                     </div>
 
                     <div className="flex items-start gap-6 mb-4">
-                        <MapPin className="text-cyan-400 w-6 h-6" />
+                        <MapPin className="text-cyan-600 w-6 h-6" />
                         <p>www.sedra.net.sa</p>
                     </div>
                 </div>
 
                 {/* Right: Contact Form */}
-                <div data-aos="fade-up">
-                    <h2 className="text-2xl md:text-3xl font-bold text-cyan-400 mb-6 tracking-widest">
+                <div data-aos="fade-left">
+                    <h2 className="text-2xl md:text-3xl font-bold text-gray-700 mb-6 tracking-widest">
                         GET IN TOUCH
                     </h2>
                     <form className="space-y-4">
@@ -71,7 +71,7 @@ const ContactUs = () => {
                             <input
                                 type="text"
                                 placeholder="First name"
-                                className="w-full border border-cyan-400 bg-transparent px-4 py-2 rounded outline-none text-white placeholder-gray-400 focus:ring-2 focus:ring-cyan-400"
+                                className="w-full border border-gray-7  00 bg-transparent px-4 py-2 rounded outline-none text-white placeholder-gray-500 focus:ring-2 focus:ring-cyan-500"
                             />
 
                         </div>
@@ -80,14 +80,14 @@ const ContactUs = () => {
                             <input
                                 type="text"
                                 placeholder="Last name"
-                                className="w-full border border-cyan-400 bg-transparent px-4 py-2 rounded outline-none text-white placeholder-gray-400 focus:ring-2 focus:ring-cyan-400"
+                                className="w-full border border-gray-700 bg-transparent px-4 py-2 rounded outline-none text-white placeholder-gray-500 focus:ring-2 focus:ring-cyan-500"
                             />
                         </div>
                         <div className="flex flex-col md:flex-row gap-4">
                             <input
                                 type="email"
                                 placeholder="Your Email"
-                                className="w-full border border-cyan-400 bg-transparent px-4 py-2 rounded outline-none text-white placeholder-gray-400 focus:ring-2 focus:ring-cyan-400"
+                                className="w-full border border-gray-700 bg-transparent px-4 py-2 rounded outline-none text-white placeholder-gray-500 focus:ring-2 focus:ring-cyan-500"
                             />
 
                         </div>
@@ -96,18 +96,18 @@ const ContactUs = () => {
                             <input
                                 type="tel"
                                 placeholder="Phone number"
-                                className="w-full border border-cyan-400 bg-transparent px-4 py-2 rounded outline-none text-white placeholder-gray-400 focus:ring-2 focus:ring-cyan-400"
+                                className="w-full border border-gray-700 bg-transparent px-4 py-2 rounded outline-none text-white placeholder-gray-500 focus:ring-2 focus:ring-cyan-500"
                             />
                         </div>
                         <textarea
                             rows={5}
                             placeholder="Message"
-                            className="w-full border border-cyan-400 bg-transparent px-4 py-2 rounded outline-none text-white placeholder-gray-400 focus:ring-2 focus:ring-cyan-400"
+                            className="w-full border border-gray-700 bg-transparent px-4 py-2 rounded outline-none text-white placeholder-gray-500 focus:ring-2 focus:ring-cyan-500"
                         ></textarea>
 
                         <button
                             type="submit"
-                            className="mt-4 px-6 py-2 bg-cyan-400 text-blue-900 font-semibold rounded shadow hover:bg-cyan-300 transition"
+                            className="mt-4 px-8 py-2 bg-[#4763ad] font-semibold rounded shadow hover:bg-[#5b75be] text-white transition"
                         >
                             SEND
                         </button>
